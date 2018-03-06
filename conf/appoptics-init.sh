4af4f4c09ce2400a0c1b36b4d7686e9f88a8ab45
Chris Rust <crust4@gmail.com>
2018-03-06 14:16:55 -0600

    Adjust init script to fail on errors and use exec to pass SIG(nals) down to the AO process

--- conf/appoptics-init.sh
+++ conf/appoptics-init.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # chris.rust@solarwinds.com - 20180227
+set -e
 
 CONFIG_FILE='/opt/appoptics/etc/config.yaml'
 TMP_FILE='/opt/appoptics/etc/config.yaml.tmp'
@@ -9,4 +10,4 @@ if [ -n "$APPOPTICS_NODE_NAME" ]; then
     cp $TMP_FILE $CONFIG_FILE
 fi
 
-/opt/appoptics/sbin/snapteld --config $CONFIG_FILE
+exec /opt/appoptics/sbin/snapteld --config $CONFIG_FILE
