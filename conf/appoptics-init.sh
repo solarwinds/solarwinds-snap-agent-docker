617dcd1b648e5b539b5a0ab548276d30d0665163
Chris Rust <crust4@gmail.com>
2018-03-27 22:13:17 -0500

    Change APPOPTICS_NODE_NAME to APPOPTICS_HOSTNAME to match AppOptics verbage

--- conf/appoptics-init.sh
+++ conf/appoptics-init.sh
@@ -5,8 +5,9 @@ set -e
 CONFIG_FILE='/opt/appoptics/etc/config.yaml'
 TMP_FILE='/opt/appoptics/etc/config.yaml.tmp'
 
-if [ -n "$APPOPTICS_NODE_NAME" ]; then
-    sed "s/# hostname_alias: myhostname/hostname_alias: $APPOPTICS_NODE_NAME/" $CONFIG_FILE > $TMP_FILE
+# If APPOPTICS_HOSTNAME is set in env, then use it as a hostname_alias in CONFIG_FILE
+if [ -n "$APPOPTICS_HOSTNAME" ]; then
+    sed "s/# hostname_alias: myhostname/hostname_alias: $APPOPTICS_HOSTNAME/" $CONFIG_FILE > $TMP_FILE
     cp $TMP_FILE $CONFIG_FILE
 fi
 
