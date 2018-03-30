3fd82ebb6cc4afe73b61950614b8a8b5be49272e
Chris Rust <crust4@gmail.com>
2018-03-29 23:52:17 -0500

    Read TOKEN from ENV to avoid having a seperate config resource, add LOG_LEVEL support, handle which plugins are enabled/disabled

--- conf/appoptics-init.sh
+++ conf/appoptics-init.sh
@@ -5,10 +5,54 @@ set -e
 CONFIG_FILE='/opt/appoptics/etc/config.yaml'
 TMP_FILE='/opt/appoptics/etc/config.yaml.tmp'
 
-# If APPOPTICS_HOSTNAME is set in env, then use it as a hostname_alias in CONFIG_FILE
-if [ -n "$APPOPTICS_HOSTNAME" ]; then
-    sed "s/# hostname_alias: myhostname/hostname_alias: $APPOPTICS_HOSTNAME/" $CONFIG_FILE > $TMP_FILE
+# APPOPTICS_TOKEN is required
+if [ -n "${APPOPTICS_TOKEN}" ]; then
+    sed "s/APPOPTICS_TOKEN/${APPOPTICS_TOKEN}/" $CONFIG_FILE > $TMP_FILE
+    cp $TMP_FILE $CONFIG_FILE
+else
+    echo "Please set APPOPTICS_TOKEN."
+    exit 1
+fi
+
+# Translate LOG_LEVEL to snapteld log_level 1-5
+if [ -n "${LOG_LEVEL}" ]; then
+    shopt -s nocasematch           # turn on case-insensitive matching for case stmt
+    case $LOG_LEVEL in
+        debug) loglevel=1;;
+        info) loglevel=2;;
+        warn|warning) loglevel=3;;
+        error) loglevel=4;;
+        fatal) loglevel=5;;
+        *) loglevel=3;;
+    esac
+    shopt -u nocasematch
+
+    sed "s/^#log_level: 3/log_level: ${loglevel}/" $CONFIG_FILE > $TMP_FILE
     cp $TMP_FILE $CONFIG_FILE
 fi
+
+# Use APPOPTICS_HOSTNAME as hostname_alias
+if [ -n "$APPOPTICS_HOSTNAME" ]; then
+    sed "s/# hostname_alias: myhostname/hostname_alias: ${APPOPTICS_HOSTNAME}/" $CONFIG_FILE > $TMP_FILE
+    cp $TMP_FILE $CONFIG_FILE
+fi
+
+# Set to true to enable or disable specific plugins
+if [ "$APPOPTICS_ENABLE_DOCKER" = "true" ]; then
+    mv /tmp/appoptics-configs/docker.yaml /opt/appoptics/etc/plugins.d/docker.yaml
+fi
+
+if [ "$APPOPTICS_ENABLE_KUBERNETES" = "true" ]; then
+    mv /tmp/appoptics-configs/kubernetes.yaml /opt/appoptics/etc/plugins.d/kubernetes.yaml
+fi
+
+if [ "$APPOPTICS_DISABLE_HOSTAGENT" = "true" ]; then
+    rm /opt/appoptics/autoload/snap-plugin-collector-aosystem
+    rm /opt/appoptics/autoload/task-aosystem-warmup.yaml
+    rm /opt/appoptics/autoload/task-aosystem.yaml
+fi
+
+# Cleanup $TMP_FILE
+[ -f "$TMP_FILE" ] && rm $TMP_FILE
 
 exec /opt/appoptics/sbin/snapteld --config $CONFIG_FILE
